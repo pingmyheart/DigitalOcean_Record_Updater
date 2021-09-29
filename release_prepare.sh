a797f2072cd6aec48fe79b287324d5171bb20c2b
Antonio Russi <antoniorussi1972@gmail.com>
2021-09-29 09:55:26 +0200

    [ADD] release_prepare.sh

---
+++ release_prepare.sh
@@ -0,0 +1,14 @@
+#create release branch from develop
+release=$(xmlstarlet sel -t -v //_:project/_:version pom.xml)
+branch="release/v${release}"
+echo -e ${branch}
+
+git checkout develop
+git checkout -b ${branch} develop
+git push -u origin release/v1.0.0
+#merge develop on master
+git checkout master
+git merge develop
+git push origin master
+git checkout ${branch}
+./dockerhub_deploy.sh
